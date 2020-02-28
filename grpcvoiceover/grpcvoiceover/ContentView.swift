dd16a10c9fb735221a2ea17891756cea92fba59e
hmarui66 <hmarui66@gmail.com>
2020-02-28 12:08:57 +0900

    add ios project

---
+++ grpcvoiceover/grpcvoiceover/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grpcvoiceover
+//
+//  Created by 丸井弘嗣 on 2020/02/28.
+//  Copyright © 2020 hmarui66. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
